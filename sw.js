a3c7b3bfe1c2daad5be13e1f0aac70a4a1f9bf66
Saiful Alam <saiful.rakib@dh.health>
2020-05-20 15:44:59 +0600

    Add PWA to the application.

---
+++ sw.js
@@ -0,0 +1,36 @@
+const cacheName = 'msar-note-pwa-conf-v1';
+const staticAssets = [
+    "./", 
+    "./index.html", 
+    "./favicon.ico", 
+    "./css/app.css", 
+    "./js/app.js",
+    "./images/fb.svg",
+    "./images/github.svg",
+    "./images/twitter.svg",
+    "./manifest.json",
+];
+
+// Cache all the files to make a PWA
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      return cache.addAll(staticAssets);
+    })
+  );
+});
+
+// Our service worker will intercept all fetch requests
+// and check if we have cached the file
+// if so it will serve the cached file
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
