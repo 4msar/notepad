6891dbb8cb2e9b45af34bfc6c994722b329f8432
Saiful Alam Rakib <saiful.rakib@miros-group.com>
2023-10-09 17:47:59 +0600

    removed unneccessary codes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,26 +4,24 @@ import Layout from "../components/Layout";
 import { getLastOpenId } from "../utils";
 
 export default function Home() {
-	const lastId = getLastOpenId();
-	const navigate = useNavigate();
+    const lastId = getLastOpenId();
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		if (
-			lastId &&
-			// eslint-disable-next-line no-restricted-globals
-			confirm("You have a last edited note,\nDo you want to open it?")
-		) {
-			navigate(`/n/${lastId}`);
-		} else {
-			navigate(`/new`);
-		}
-	}, [navigate, lastId]);
+    useEffect(() => {
+        if (
+            lastId &&
+            // eslint-disable-next-line no-restricted-globals
+            confirm("You have a last edited note,\nDo you want to open it?")
+        ) {
+            navigate(`/n/${lastId}`);
+        } else {
+            navigate(`/new`);
+        }
+    }, [navigate, lastId]);
 
-	console.log('Home', { lastId });
-
-	return (
-		<Layout>
-			<div className="flex-center loading">Loading...</div>
-		</Layout>
-	);
+    return (
+        <Layout>
+            <div className="flex-center loading">Loading...</div>
+        </Layout>
+    );
 }
