da4466ce356d6067f070a09463306df9ff8e1241
Saiful Alam Rakib <saiful.rakib@miros-group.com>
2023-10-10 01:31:27 +0600

    Updated service worker

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
+            outDir: "dist",
             registerType: "autoUpdate",
             injectRegister: "auto",
             workbox: {
