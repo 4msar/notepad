018bd9812d6a92555bd37931e825408a6f438f22
Saiful Alam Rakib <msa4rakib@gmail.com>
2023-10-11 15:54:07 +0600

    Updated for mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export function Layout({
     children: React.ReactNode;
 } & Partial<NavbarProps>) {
     return (
-        <div className="bg-white dark:bg-slate-700 font-nunito dark:border-slate-900 border rounded-md m-0 sm:m-20 sm:mx-auto w-full sm:w-[80vw] h-full">
+        <div className="bg-white dark:bg-slate-700 font-nunito dark:border-slate-900 border-none sm:border rounded-md m-0 sm:m-20 sm:mx-auto w-full sm:w-[80vw] h-full">
             <Navbar hideNavLinks={hideNavLinks} {...(props as NavbarProps)} />
             <main className="relative mx-auto max-h-min">{children}</main>
             <Footer hideMeta={hideMeta} />
