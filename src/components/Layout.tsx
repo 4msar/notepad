90a1f05d4fa3a59ab50222ed719382b7d65cf946
Saiful Alam Rakib <saiful.rakib@miros-group.com>
2023-10-11 16:25:58 +0600

    Updated border design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ export function Layout({
     return (
         <div className="bg-white dark:bg-slate-700 font-nunito rounded-none sm:rounded-md m-0 sm:m-20 sm:mx-auto w-full sm:w-[80vw] h-full">
             <Navbar hideNavLinks={hideNavLinks} {...(props as NavbarProps)} />
-            <main className="relative mx-auto border-none sm:border-x dark:border-slate-800 max-h-min">
+            <main className="relative mx-auto border-x-0 sm:border-x dark:border-slate-800 max-h-min">
                 {children}
             </main>
             <Footer hideMeta={hideMeta} />
