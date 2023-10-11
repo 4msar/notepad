142d8da45ac5cf877e3de9f81724a1a34f0c66b2
Saiful Alam Rakib <msa4rakib@gmail.com>
2023-10-11 15:32:48 +0600

    Added pwa options

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,26 @@ export default defineConfig({
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
             },
+            includeAssets: ["favicon.ico", "logo128.png", "logo512.png"],
+            manifest: {
+                name: "Noto - Note Taking App",
+                short_name: "Noto",
+                description:
+                    "Simple note taking application with realtime sync.",
+                theme_color: "#94a3b8",
+                icons: [
+                    {
+                        src: "logo128.png",
+                        sizes: "128x128",
+                        type: "image/png",
+                    },
+                    {
+                        src: "logo512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
         }),
     ],
     resolve: {
