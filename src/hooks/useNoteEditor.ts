19cb9a9ac3123e88852d0c2c82ff73d711a8a0bf
Saiful Alam Rakib <saiful.rakib@miros-group.com>
2023-10-11 16:53:40 +0600

    Updated min height for the editor

--- src/hooks/useNoteEditor.ts
+++ src/hooks/useNoteEditor.ts
@@ -10,7 +10,7 @@ type UseNoteEditorProps = {
 };
 
 export const rootClassName = clsx(
-    "relative max-h-full h-full min-h[60vh] sm:max-h-[80vh] sm:h-[calc(100vh-15rem)] w-full text-slate-900 dark:text-white px-2 point-bg"
+    "relative max-h-full h-full min-h-[60vh] sm:max-h-[80vh] sm:h-[calc(100vh-15rem)] w-full text-slate-900 dark:text-white px-2 point-bg"
 );
 
 export function useNoteEditor(
